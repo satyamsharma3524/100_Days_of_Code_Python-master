cd889a368f11e9ba12adb3ebb200bc86643e9a82
Satyam Sharma <satyamsharma3524@gmail.com>
2022-03-09 22:21:18 +0530

    day_27 with tkinter module and arguments.

--- day_27/tkinter_gui.py
+++ day_27/tkinter_gui.py
@@ -1,1 +1,11 @@
 import tkinter
+
+window = tkinter.Tk()
+
+window.title("My first GUI program")
+window.minsize(width=500, height=500)
+
+my_label = tkinter.Label(text="This is my label", font=("arial", 24, "bold"))
+my_label.pack()
+
+window.mainloop()
